b7f3db290f8cb3c9bc1604ebf6de739119fa06d9
小楼临风 <ruyetao@gmail.com>
2013-05-29 16:54:49 +0800

    Update and rename test.py to p75.py

---
+++ p75.py
@@ -0,0 +1,15 @@
+number = input('Enter a number between 1 and 10: ')
+if number <= 10 and number >= 1:
+    print 'Great!'
+else:
+    print 'Wrong!'
+
+#复杂的写法，不推荐
+#number = input('Enter a number between 1 and 10: ')
+#if number <= 10:
+#    if number >= 1:
+#        print 'Great!'
+#    else:
+#        print 'Wrong!'
+#else:
+#    print 'Wrong!'
